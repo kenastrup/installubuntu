781c5b296e7c8cb416378d8aae607627b486f109
Ken Are Astrup <ken.astrup@gmail.com>
2014-07-29 14:42:08 +0200

    endelig løsning

--- installasjon-programvare-1404.sh
+++ installasjon-programvare-1404.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 sudo apt-get update
+sudo apt-get upgrade
 
-sudo apt-get install aurora-maven3 aurora-default-java-switcher aurora-rabbitvcs aurora-jdk-1.7.0u60 aurora-jdk-1.8.0 aurora-ske-certificates aurora-liquibase-3.0.6 subversion svnkit git git-svn
+sudo apt-get install aurora-jdk-1.7.0u60 aurora-jdk-1.8.0 aurora-ske-certificates subversion svnkit git git-svn aurora-maven3 aurora-default-java-switcher aurora-rabbitvcs 
 
 # kommentert vekk. trenger ikke?
-#sudo apt-get install sqldeveloper aurora-soapui-5.0.0
+#sudo apt-get install sqldeveloper aurora-soapui-5.0.0 aurora-liquibase-3.0.6 
 
 sudo aurora-default-java-switcher
 
@@ -15,12 +16,15 @@ sudo apt-get install aurora-node-0.10.25
 mkdir ~/npm
 npm config set prefix ~/npm
 
+npm install yo
+
+echo PATH=$PATH:$HOME/npm/bin >> .profile
+
 git config --global user.name "Ken Are Astrup"
 git config --global user.email "ken.astrup@gmail.com" 
-
-
+git config --global url.https://.insteadOf git://
 
 sudo apt-get install ruby
 sudo gem install compass
 
-sudo shutdown -r now
+sudo reboot now
